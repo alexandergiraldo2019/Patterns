13a7e385fda6cd32325df77d95f3d620df0fdd8e
Tecnologia <Tecnologia@DESKTOP-4NREQDL>
2021-02-04 18:50:14 -0500

    Agregar archivos de proyecto.

---
+++ WebApi/Controllers/AlbumesController.cs
@@ -0,0 +1,45 @@
+﻿using DataAccess.Generic;
+using Entities.Domain;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AlbumesController : ControllerBase
+    {
+        private readonly IGenericRepository<Album> _genericRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AlbumesController(IGenericRepository<Album> genericRepository, IUnitOfWork unitOfWork) 
+        {
+            this._genericRepository = genericRepository;
+            this._unitOfWork = unitOfWork;
+        } 
+
+        [HttpGet]
+        public async Task<IEnumerable<Album>> Get()
+        {
+            return await _genericRepository.GetAsync(a => a.Anio > 1990, a => a.OrderByDescending(b => b.Anio), "Artista");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Album album)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var created = await _genericRepository.CreateAsync(album);
+
+            if(created)
+                _unitOfWork.Commit();
+
+            return Created("Created", new { Response = StatusCode(201) });
+        }
+    }
+}
